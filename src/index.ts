1b5f02ec6c30fba6e865a54b6b13ba9915152788
Dennis Gera <dennis.gera@bain.com>
2025-07-15 13:37:11 -0300

    feat: add middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,23 @@ const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
 
+const middlewareLogResponses = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void => {
+  res.on("finish", () => {
+    if (res.statusCode >= 400) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+  next();
+};
+
+app.use(middlewareLogResponses);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
