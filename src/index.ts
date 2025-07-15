64e8ce467785b855b1d7c5d2d96b8d30f19de278
Dennis Gera <dennis.gera@bain.com>
2025-07-15 13:45:23 -0300

    feat: implement middleware counter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import config from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+const middlewareMetricsInc = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void => {
+  config.fileserverHits++;
+  next();
+};
 
 const middlewareLogResponses = (
   req: express.Request,
@@ -20,13 +28,25 @@ const middlewareLogResponses = (
   next();
 };
 
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
 app.use(middlewareLogResponses);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.get("/healthz", (req, res) => {
+app.get("/healthz", (req: express.Request, res: express.Response) => {
   res.set("Content-Type", "text/plain");
+  res.send("OK");
+});
+
+app.get("/metrics", (req: express.Request, res: express.Response) => {
+  res.set("Content-Type", "text/plain");
+  res.send(`Hits: ${config.fileserverHits}`);
+});
+
+app.get("/reset", (req: express.Request, res: express.Response) => {
+  config.fileserverHits = 0;
   res.send("OK");
 });
