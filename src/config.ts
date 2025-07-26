33605ffc14f840ef8c6d6554faf73e18be91cfc3
Dennis Gera <dennis.gera@bain.com>
2025-07-26 16:35:37 -0300

    feat: add drizzle

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,21 @@
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+  const value = process.env.key;
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+}
+
 type APIConfig = {
   fileserverHits: number;
+  dbUrl: string;
 };
 
 const config: APIConfig = {
   fileserverHits: 0,
+  dbUrl: envOrThrow("DB_URL"),
 };
 
 export default config;
