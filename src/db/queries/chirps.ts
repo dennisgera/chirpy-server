47b8904a37fcc7bfb89f5ebfd648f704861e22a5
Dennis Gera <dennis.gera@bain.com>
2025-08-03 16:19:11 -0300

    feat: add return type

--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -1,7 +1,7 @@
 import { db } from "../index.js";
 import { NewChirp, chirps } from "../schema.js";
 
-export async function createChirp(chirp: NewChirp) {
+export async function createChirp(chirp: NewChirp): Promise<NewChirp> {
   const [result] = await db.insert(chirps).values(chirp).returning();
   return result;
 }
--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,7 +1,7 @@
 import { db } from "../index.js";
 import { NewUser, users } from "../schema.js";
 
-export async function createUser(user: NewUser) {
+export async function createUser(user: NewUser): Promise<NewUser> {
   const [result] = await db
     .insert(users)
     .values(user)
